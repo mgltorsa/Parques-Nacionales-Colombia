920471a15abd316e38faad8b572cfeb1d6493956
miguelito657 <35902662+miguelito657@users.noreply.github.com>
2018-02-03 20:16:08 -0500

    Se sube Todo

---
+++ View/View/Main.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace View
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.map = new GMap.NET.WindowsForms.GMapControl();
+            this.SuspendLayout();
+            // 
+            // map
+            // 
+            this.map.Bearing = 0F;
+            this.map.CanDragMap = true;
+            this.map.EmptyTileColor = System.Drawing.Color.Navy;
+            this.map.GrayScaleMode = false;
+            this.map.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
+            this.map.LevelsKeepInMemmory = 5;
+            this.map.Location = new System.Drawing.Point(219, 13);
+            this.map.MarkersEnabled = true;
+            this.map.MaxZoom = 2;
+            this.map.MinZoom = 2;
+            this.map.MouseWheelZoomEnabled = true;
+            this.map.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+            this.map.Name = "map";
+            this.map.NegativeMode = false;
+            this.map.PolygonsEnabled = true;
+            this.map.RetryLoadTile = 0;
+            this.map.RoutesEnabled = true;
+            this.map.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
+            this.map.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
+            this.map.ShowTileGridLines = false;
+            this.map.Size = new System.Drawing.Size(384, 339);
+            this.map.TabIndex = 0;
+            this.map.Zoom = 0D;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(637, 434);
+            this.Controls.Add(this.map);
+            this.Name = "Main";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private GMap.NET.WindowsForms.GMapControl map;
+    }
+}
+
---
+++ View/View/Main.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GMap.NET;
+using GMap.NET.WindowsForms;
+using System.IO;
+using GMap.NET.MapProviders;
+
+
+
+namespace View
+{
+    public partial class Main : Form
+    {
+        public const double INIT_LAT = 4.0000000;
+        public const double INIT_LNG = -72.0000000;
+        public const string LIMITS_PATH = "..//..//Resources/LimitesParques.csv";
+        const string PATTERN = @"\[([^\]]*)\]";
+
+
+
+        public Main()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            map.DragButton = MouseButtons.Left;
+            map.CanDragMap = true;
+            map.Position = new PointLatLng(INIT_LAT, INIT_LNG);
+            map.MapProvider = GMapProviders.GoogleTerrainMap;
+            map.MinZoom = 0;
+            map.MaxZoom = 24;
+            map.Zoom = 9;
+            map.AutoScroll = true;
+
+            LoadGeoPolygon();
+        }
+
+        private void LoadGeoPolygon()
+        {
+            StreamReader sr = new StreamReader(path: LIMITS_PATH);
+
+            SaveFileDialog s = new SaveFileDialog();
+            s.ShowDialog();
+
+            StreamWriter sw = new StreamWriter(s.OpenFile());
+            sr.ReadLine();
+
+            List<PointLatLng> points = new List<PointLatLng>();
+
+            string line = sr.ReadLine();
+            string[] info = line.Split(',');
+            int count = 3;
+            for (int i = count; i < info.Length; i++)
+            {
+                string infoCoordinates = info[i].Trim();
+                string[] parCoordinates = infoCoordinates.Split(' ');
+
+                double lng = 0;
+                double lat = 0;
+                if (parCoordinates.Length>1)
+                {
+                    Double.TryParse(parCoordinates[0], out lng);
+                    Double.TryParse(parCoordinates[1], out lat);
+                }
+
+                if (lng == 0 || lat == 0)
+                {
+                    count = i;
+                    break;
+                }
+                else
+                {
+                    sw.WriteLine(parCoordinates[0] + " " + parCoordinates[1]);
+                    points.Add(new PointLatLng(lat, lng));
+                }
+            }
+
+
+
+
+            sw.Close();
+
+            GMapOverlay polygonOverlay = new GMapOverlay("PolygonOverlay");
+            GMapPolygon polygon = new GMapPolygon(points, "Polygon1")
+            {
+                Fill = Brushes.Black
+            };
+            polygonOverlay.Polygons.Add(polygon);
+            map.Overlays.Add(polygonOverlay);
+            map.Zoom += 1;
+            map.Zoom -= 1;
+        }
+
+        private string[] ConvertToLimitCoor(string[] coorPolygon)
+        {
+            string[] polygon = new string[coorPolygon.Length];
+            for (int i = 0; i < coorPolygon.Length; i++)
+            {
+                polygon[i] = coorPolygon[i].Trim();
+            }
+
+            return polygon;
+        }
+
+
+
+        private string GetMultyPolygonLine(string[] infoLine)
+        {
+            return null;
+        }
+    }
+}
